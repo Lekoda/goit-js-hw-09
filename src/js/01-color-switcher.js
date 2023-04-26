1e5a4b98df066b75aa43c983de0756feac8180cd
Valeria Otchenash <kratorchestra@gmail.com>
2023-04-26 21:14:04 +0300

    add 01-color-switch.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+    colorWidget: document.querySelector('body'),
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+};
+let intervalID = null;
 
+refs.startBtn.addEventListener('click', () => {
+    if (intervalID === null) {
+        intervalID = setInterval(() => {
+            refs.colorWidget.style.backgroundColor = getRandomHexColor();
+        }, 1000);
+
+        refs.startBtn.disabled = true;
+    }
+});
+
+refs.stopBtn.addEventListener('click', () => {
+    clearInterval(intervalID);
+    refs.startBtn.disabled = false;
+    intervalID = null;
+});
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
