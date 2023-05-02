9f3e3122fef1311158c32270ae41556a66bc5946
Valeria Otchenash <kratorchestra@gmail.com>
2023-05-02 16:03:45 +0300

    add UDP 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,10 +2,12 @@ import Notiflix from 'notiflix';
 import "notiflix/dist/notiflix-3.2.6.min.css";
 
 
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   
   return new Promise((resolve, reject) => {
+    
     setTimeout(() => {
       const result = { position, delay };
       if (shouldResolve) {
@@ -16,23 +18,26 @@ function createPromise(position, delay) {
     }, delay);
   }); 
 }
-
 const formEl = document.querySelector('.form');
 
 formEl.addEventListener('submit', (evt) => {
   evt.preventDefault();
-
+  
   const delay = parseInt(formEl.elements.delay.value);
   const step = parseInt(formEl.elements.step.value);
   const amount = parseInt(formEl.elements.amount.value);
 
-  for (let position = 1, i = 0; i < amount; position += step, i++) {
-    createPromise(position, delay)
+  for (let position = 1; position <= amount; position += 1) {
+    const currentDelay = delay + step * (position - 1);
+
+    createPromise(position, currentDelay)
       .then(({ position, delay }) => {
-        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+       Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
-        Notiflix.Notify.failure(`⛔ Rejected promise ${position} in ${delay}ms`);
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
       });
-  };
+  }
 });
+
+  
